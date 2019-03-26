1775f2563cfe409f9336f0bfa8e8ca24d6d63eb3
Dasom Kim <dev.dasom1012@gmail.com>
2019-03-26 11:25:42 +0900

    favicon 변경, 리덕스 개발자도구 비활성화

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
@@ -11,14 +11,12 @@ import './styles/components/index.scss';
 
 
 // 개발자 도구 사용 설정
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const composeEnhancers = devTools || compose; // 리덕스 개발자도구가 설치되어있지 않다면 일반 compose 를 사용
+// const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+// const composeEnhancers = devTools || compose; // 리덕스 개발자도구가 설치되어있지 않다면 일반 compose 를 사용
 
 const middlewares = [ReduxThunk]; // 미들웨어 추가
 
-const store = createStore(reducers, composeEnhancers(
-  applyMiddleware(...middlewares)
-));
+const store = createStore(reducers,  applyMiddleware(...middlewares));
 
 
 ReactDOM.render(
