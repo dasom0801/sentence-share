9d6e7e0b75f58a3eaa1dfce6d792483653c7b979
dasom.kim <dev.dasom1012@gmail.com>
2024-02-22 16:08:23 +0900

    feat(Spinner): implement Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,6 @@
+import { CgSpinner } from 'react-icons/cg';
+
+const Spinner = () => {
+  return <CgSpinner className='animate-spin text-3xl' />;
+};
+export default Spinner;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import MaxWidthWrapper from './MaxWidthWrapper';
 import AuthGuard from './AuthGuard';
 import Button from './Button';
 import Input from './Input';
+import Spinner from './Spinner';
 
-export { Logo, MaxWidthWrapper, AuthGuard, Button, Input };
+export { Logo, MaxWidthWrapper, AuthGuard, Button, Input, Spinner };
