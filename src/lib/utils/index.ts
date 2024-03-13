fc7d8b703d17303a47db812ad5ab3fca81bea890
Dasom <dev.dasom1012@gmail.com>
2024-03-13 15:42:23 +0900

    refactor: Remove unused libraries

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,11 +1,5 @@
 import { createTheme } from '@mui/material';
 import { blueGrey } from '@mui/material/colors';
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
 
 // MUI 컴포넌트 theme 설정
 export const customTheme = () => {
