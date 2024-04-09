99fe480c43a71022f60e698d20150a19b2859f56
Dasom <dev.dasom1012@gmail.com>
2024-04-10 07:30:56 +0900

    refactor(useSort): state 제거, searchParams로 값 변경

--- src/lib/hooks/useSort.ts
+++ src/lib/hooks/useSort.ts
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { SortBy, SortOrder } from '@/types/enum';
 
 import {
   getSearchParamsObject,
@@ -10,48 +9,34 @@ import {
 
 const useSort = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [sort, setSort] = useState<SortType>({
-    sortBy: SortBy.CreatedAt,
-    sortOrder: SortOrder.DESC,
-  });
-  const [initial, setInitial] = useState(true);
-  // params가 바뀌면 state에 반영
-  useEffect(() => {
-    setSearchParams((prev: URLSearchParams) => {
-      // params가 있는 주소로 접속한 경우 state의 초기값으로 params를 변경하지 않기 위함.
-      if (initial) {
-        setInitial(false);
-        return prev;
-      }
-      const prevParams = getSearchParamsObject(prev);
-      const { sortBy, sortOrder } = sort;
-      if (
-        prevParams.sortBy !== sortBy ||
-        getSortOrderValue(prevParams.sortOrder) !== sortOrder
-      ) {
-        return { ...prevParams, sortBy, sortOrder: `${sortOrder}` };
-      }
 
-      return prev;
-    });
-  }, [sort]);
+  // 선택한 sort를 주소에 반영
+  const setSort = useCallback(
+    (value: string) => {
+      const searchPramsObject = getSearchParamsObject(searchParams);
+      const [sortByValue, sortOrderValue] = value.split('=');
+      const sortBy = getSortByValue(sortByValue) as string;
+      const sortOrder = getSortOrderValue(sortOrderValue).toString();
+      setSearchParams({ ...searchPramsObject, sortBy, sortOrder });
+    },
+    [searchParams, setSearchParams],
+  );
 
-  useEffect(() => {
-    // 상태가 변경될 때 params 업데이트
-    const params = getSearchParamsObject(searchParams);
-    if (
-      !sort ||
-      params.sortBy !== sort?.sortBy ||
-      getSortOrderValue(params.sortOrder) !== sort?.sortOrder
-    ) {
-      setSort({
-        sortBy: getSortByValue(params.sortBy),
-        sortOrder: getSortOrderValue(params.sortOrder),
-      });
-    }
+  // 현재 선택된 sort를 문자열로 반환
+  const currentSort = useMemo(() => {
+    const sortBy = searchParams.get('sortBy');
+    const sortOrder = searchParams.get('sortOrder');
+    return `${sortBy}=${sortOrder}`;
   }, [searchParams]);
 
-  return { sort, setSort };
+  // API 요청을 위한 sort 값을 객체로 반환
+  const sortParams = useMemo(() => {
+    const sortBy = getSortByValue(searchParams.get('sortBy'));
+    const sortOrder = getSortOrderValue(searchParams.get('sortOrder'));
+    return { sortBy, sortOrder };
+  }, [searchParams]);
+
+  return { currentSort, sortParams, setSort };
 };
 
 export default useSort;
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -8,16 +8,16 @@ export const getBearerToken = () => {
 
 // searchPrams를 object로 변환
 export const getSearchParamsObject = (
-  searchParams: URLSearchParams
+  searchParams: URLSearchParams,
 ): { [key: string]: string } => {
   return Array.from(searchParams.entries()).reduce(
     (acc, [key, value]) => ({ ...acc, [key]: value }),
-    {}
+    {},
   );
 };
 
 // SortBy 타입에 맞는 값으로 전달
-export const getSortByValue = (sortBy: string): SortBy => {
+export const getSortByValue = (sortBy: string | null): SortBy => {
   switch (sortBy) {
     case 'createdAt':
       return SortBy.CreatedAt;
@@ -28,7 +28,7 @@ export const getSortByValue = (sortBy: string): SortBy => {
   }
 };
 
-export const getSortOrderValue = (sortOrder: string): SortOrder => {
+export const getSortOrderValue = (sortOrder: string | null): SortOrder => {
   switch (sortOrder) {
     case '-1':
       return SortOrder.DESC;
