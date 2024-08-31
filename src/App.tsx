e83cc040ed92be80cf5f2d07b41e14e7eace0ad9
Dasom <dev.dasom1012@gmail.com>
2024-08-31 11:37:56 +0900

    feat: 라우트 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 /** @jsxImportSource @emotion/react */
 
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from '@mui/material';
 import { css } from '@emotion/react';
 
-import { GlobalStyles, Header, ErrorResult } from '@/components';
+import { AuthGuard, ErrorResult, GlobalStyles, Header } from '@/components';
 import { ErrorBoundary } from 'react-error-boundary';
 import { MUI_THEME } from './constants';
+import { MainPage } from '@/feature/main';
+import { SentenceDetailPage } from '@/feature/sentence-detail';
+import { BookDetailPage } from '@/feature/book-detail';
+import { SettingPage } from '@/feature/setting';
+import { UserSentencePage } from '@/feature/user-sentence';
+import { UserLikePage } from '@/feature/user-like';
+import { SentenceEditPage } from '@/feature/sentence-edit';
 
 const queryClient = new QueryClient();
+const Router = () => (
+  <Routes>
+    <Route path="/">
+      <Route path="/" element={<MainPage />} />
+      <Route path="/sentence/:id" element={<SentenceDetailPage />} />
+      <Route path="/book/:id" element={<BookDetailPage />} />
+      <Route element={<AuthGuard />}>
+        <Route path="/my/setting" element={<SettingPage />} />
+        <Route path="/my/sentence" element={<UserSentencePage />} />
+        <Route path="/my/like" element={<UserLikePage />} />
+        <Route path="/edit/sentence" element={<SentenceEditPage />} />
+        <Route path="/edit/sentence/:id" element={<SentenceEditPage />} />
+      </Route>
+    </Route>
+  </Routes>
+);
 
 const App = () => {
   return (
@@ -24,16 +47,18 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={MUI_THEME}>
           <GlobalStyles />
-          <Header />
-          <div
-            css={css`
-              padding-top: 56px;
-            `}
-          >
-            <ErrorBoundary FallbackComponent={ErrorResult}>
-              <Outlet />
-            </ErrorBoundary>
-          </div>
+          <BrowserRouter>
+            <Header />
+            <div
+              css={css`
+                padding-top: 56px;
+              `}
+            >
+              <ErrorBoundary FallbackComponent={ErrorResult}>
+                <Router />
+              </ErrorBoundary>
+            </div>
+          </BrowserRouter>
         </ThemeProvider>
 
         <Toaster />
