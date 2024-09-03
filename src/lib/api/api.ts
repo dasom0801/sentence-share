6360977666d35000aedc5957d0d43ad73ad8fb6d
Dasom <dev.dasom1012@gmail.com>
2024-09-04 08:59:59 +0900

    feat: fetch 사용을 위한 유틸 정의

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -2,6 +2,31 @@ import axios from 'axios';
 import { getBearerToken } from '../utils';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const fetchAPI = async <T>(
+  url: string,
+  init?: RequestInit,
+): Promise<T> => {
+  const token = await getBearerToken();
+  let headers = {};
+  if (token) {
+    headers = { Authorization: token };
+  }
+  if (init) {
+    init = { ...init, ...headers };
+  }
+  try {
+    const response = await fetch(`${BASE_URL}${url}`, init);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error('요청에 실패했습니다');
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 axiosInstance.interceptors.request.use((config) => {
   const authorization = getBearerToken();
