f98fa1ea53c02c858e6d9a9760bb9577fbe23fbd
Dasom <dev.dasom1012@gmail.com>
2024-09-04 23:14:16 +0900

    refactor: 최상위 layout에서 Header 표시

--- src/app/book/layout.tsx
+++ src/app/book/layout.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Header from '@components/common/header/index';
 
 export default function BookLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Header />
-      <div style={{ paddingTop: 56 }}>{children}</div>
-    </>
-  );
+  return <>{children}</>;
 }
