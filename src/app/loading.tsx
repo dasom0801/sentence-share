5177265d6625cb2eeb2f65cebd28ce73b34a5963
Dasom <dev.dasom1012@gmail.com>
2024-09-07 15:38:16 +0900

    refactor: 로딩 화면 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,12 @@
 import SentenceListSkeleton from '@components/sentence/sentence-list-skeleton';
 import MaxWidthWrapper from '@components/common/max-width-wrapper';
+import classes from './loading.module.scss';
 
 export default function MainLoading() {
   return (
     <main>
       <MaxWidthWrapper>
+        <div className={classes.buttons}></div>
         <SentenceListSkeleton />
       </MaxWidthWrapper>
     </main>
