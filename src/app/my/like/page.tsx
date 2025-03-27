05d9fe9b9b815546ddd455cd0913679c58eb8760
Dasom <dev.dasom1012@gmail.com>
2025-03-27 17:23:41 +0900

    fix: 좋아요 한 목록 가져오기

--- src/app/my/like/page.tsx
+++ src/app/my/like/page.tsx
@@ -1,8 +1,7 @@
+import MaxWidthWrapper from '@components/common/max-width-wrapper';
 import { Metadata } from 'next';
-import { getUser } from '@/lib/api';
 import MyLikeList from './_components/my-like-list';
 import classes from './page.module.scss';
-import MaxWidthWrapper from '@components/common/max-width-wrapper';
 
 export const metadata: Metadata = {
   title: '내가 좋아한 문장 - Sentence Share',
@@ -15,13 +14,10 @@ type MyLikePageProps = {
 export default async function MyLikePage({
   searchParams: { page },
 }: MyLikePageProps) {
-  const currentUser = await getUser();
-
   return (
     <main className={classes.main}>
       <MaxWidthWrapper>
-        {/* @ts-expect-error Async Server Component */}
-        <MyLikeList userId={currentUser?._id} page={page} />
+        <MyLikeList page={page} />
       </MaxWidthWrapper>
     </main>
   );
