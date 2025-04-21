e51c45e63a792c21c0c49c20ed9b19f0fb7382b0
Dasom <dev.dasom1012@gmail.com>
2025-04-21 18:52:05 +0900

    chore: 사용하지 않는 파일 삭제 (테스트 코드는 나중에 재시도)

--- src/mocks/data/index.ts
+++
@@ -1,5 +0,0 @@
-import MockBook from './book.json';
-import MockSentence from './sentence.json';
-import MockUser from './user.json';
-import MockSentences from './sentences.json';
-export { MockBook, MockSentence, MockUser, MockSentences };
