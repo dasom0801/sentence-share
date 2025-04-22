9665a1697a2cafd4b318ef42aaa14fb9887060c8
Dasom <dev.dasom1012@gmail.com>
2025-04-22 19:08:53 +0900

    chore: mock data 복구(Storybook에서 사용)

---
+++ src/mocks/data/index.ts
@@ -0,0 +1,5 @@
+import MockBook from './book.json';
+import MockSentence from './sentence.json';
+import MockSentences from './sentences.json';
+import MockUser from './user.json';
+export { MockBook, MockSentence, MockSentences, MockUser };
