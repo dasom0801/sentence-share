4d1c26635a7c242c4e6033dc38aaf4f53169689b
Dasom <dev.dasom1012@gmail.com>
2025-04-24 14:52:02 +0900

    fix: 오타 수정

--- src/app/my/sentence/components/MySentenceList/api/getUserSentences.ts
+++ src/app/my/sentence/components/MySentenceList/api/getUserSentences.ts
@@ -4,7 +4,7 @@ import type { PaginationResult, Sentence } from '@/types';
 const SENTENCE_PAGE_LIMIT = 24;
 const getUserSentences = async (page: string) => {
   return fetchAPI<PaginationResult<Sentence>>(
-    `/users/me/sentences?page=${page || 1}&limit=${SENTENCE_PAGE_LIMIT}}`,
+    `/users/me/sentences?page=${page || 1}&limit=${SENTENCE_PAGE_LIMIT}`,
     {
       cache: 'no-store',
     },
