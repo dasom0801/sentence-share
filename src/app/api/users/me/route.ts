37b4d37c8159a8a147f866ab818ebfefc7901548
Dasom <dev.dasom1012@gmail.com>
2025-04-28 17:16:08 +0900

    fix: Dynamic Server Error 문제 해결 - cookie()를 최상위에서 호출

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,16 +1,18 @@
 import { editUserInfo, getUserInfo } from '@/db/controllers';
 import { handleError } from '@/db/utils';
 import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 // 로그인한 사용자의 정보 가져오기
 export async function GET(req: NextRequest) {
+  const token = cookies().get('access_token')?.value;
   try {
-    const user = await getUserInfo();
+    const data = token ? await getUserInfo(token) : null;
     return NextResponse.json(
       {
         success: true,
-        data: user,
+        data,
         message: '사용자 정보를 성공적으로 가져왔습니다.',
       },
       { status: 200 },
