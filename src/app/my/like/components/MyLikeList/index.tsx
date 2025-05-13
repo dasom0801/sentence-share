c1b906de6714e9c3d35b218a5e67c31e82bf212a
Dasom <dev.dasom1012@gmail.com>
2025-05-13 16:27:08 +0900

    refactor: MyLikeList 컴포넌트는 렌더링만 담당, 공통 컴포넌트 활용

--- src/app/my/like/components/MyLikeList/index.tsx
+++ src/app/my/like/components/MyLikeList/index.tsx
@@ -1,33 +1,30 @@
-import { LikeButton, Pagination } from '@/components/molecules';
-import { SentenceCard } from '@/components/organisms';
+import { Pagination } from '@/components/molecules';
+import { SentenceLikeCardList } from '@/components/organisms';
 
+import type { Sentence } from '@/types';
 import MyLikeEmpty from '../MyLikeEmpty';
 import classes from './MyLikeList.module.scss';
-import { getUserLikes } from './api';
 
 type MyLikeListProps = {
-  page: string;
+  list: Sentence[];
+  totalPages: number;
+  total: number;
 };
 
-export default async function MyLikeList({ page }: MyLikeListProps) {
-  const { data: likes } = await getUserLikes(page);
+export default async function MyLikeList({
+  list,
+  totalPages,
+  total,
+}: MyLikeListProps) {
   return (
     <>
       <h1 className={classes.title}>
-        내가 좋아한 문장 {likes?.total && `(${likes?.total})`}
+        내가 좋아한 문장 {!!total && `(${total})`}
       </h1>
-      {!!likes?.total ? (
+      {!!total ? (
         <>
-          <ul className="grid-container">
-            {likes?.list.map((sentence) => (
-              <li key={sentence._id}>
-                <SentenceCard sentence={sentence}>
-                  <LikeButton id={sentence._id} isLiked={sentence.isLiked} />
-                </SentenceCard>
-              </li>
-            ))}
-          </ul>
-          <Pagination count={likes?.totalPages} />
+          <SentenceLikeCardList list={list} />
+          <Pagination count={totalPages} />
         </>
       ) : (
         <MyLikeEmpty />
