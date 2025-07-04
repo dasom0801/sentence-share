85dea1502d3a1cbb47b7fa0a329a92d8a1fa08cb
Dasom <dev.dasom1012@gmail.com>
2025-07-04 19:07:15 +0900

    chore: a11y.test error 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from '@mui/material';
 import type { Preview } from '@storybook/nextjs-vite';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
 import '../src/styles/global.scss';
 
 import { MUI_THEME } from '../src/lib/mui/theme';
@@ -16,6 +15,9 @@ const preview: Preview = {
     },
     nextjs: {
       appDirectory: true,
+    },
+    a11y: {
+      test: 'error',
     },
   },
 
