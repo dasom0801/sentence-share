784f5ca09624aae49bd25549ab16a0e76fda8e48
Dasom <dev.dasom1012@gmail.com>
2025-07-04 23:24:57 +0900

    test: BookCoverImage 컴포넌트 단위 테스트 추가

---
+++ src/app/edit/sentence/components/BookCoverImage/BookCoverImage.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, fireEvent, waitFor } from 'storybook/test';
+import BooKCoverImage from './';
+
+const meta = {
+  title: 'page/edit/BooKCoverImage',
+  component: BooKCoverImage,
+} satisfies Meta<typeof BooKCoverImage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const defaultArgs = {
+  coverUrl:
+    'https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1652076%3Ftimestamp%3D20190131131541',
+  alt: 'Book Cover',
+};
+
+export const BookCover: Story = {
+  args: {
+    ...defaultArgs,
+  },
+  play: async ({ canvas }) => {
+    const image = canvas.getByRole('img', { name: defaultArgs.alt });
+    expect(image).toBeInTheDocument();
+    expect(image.getAttribute('src')).toContain(defaultArgs.coverUrl);
+  },
+};
+
+export const BookCoverError: Story = {
+  args: {
+    ...defaultArgs,
+    coverUrl: 'https://invalid-url.com/cover.jpg',
+  },
+  play: async ({ canvas }) => {
+    // 처음에는 책 이미지 렌더링
+    const image = canvas.getByRole('img', { name: defaultArgs.alt });
+    expect(image).toBeInTheDocument();
+
+    // 에러 발생
+    fireEvent.error(image);
+
+    // Default Cover 이미지 렌더링
+    const defaultCover = await canvas.findByTestId('book-default-cover');
+    expect(defaultCover).toBeInTheDocument();
+
+    await waitFor(() => {
+      const bookCover = canvas.queryByRole('img', { name: defaultArgs.alt });
+      expect(bookCover).not.toBeInTheDocument();
+    });
+  },
+};
--- src/app/edit/sentence/components/BookCoverImage/index.tsx
+++ src/app/edit/sentence/components/BookCoverImage/index.tsx
@@ -8,25 +8,29 @@ import classes from './BookCoverImage.module.scss';
 type BookCoverImageProps = {
   coverUrl: string;
   alt: string;
+  width?: number;
+  height?: number;
 };
 
-export default function BookCoverImage({ coverUrl, alt }: BookCoverImageProps) {
-  const [showDefaultCover, setShowDefaultCover] = useState(false);
-  return (
-    <>
-      {showDefaultCover || !coverUrl ? (
-        <BookDefaultCover />
-      ) : (
-        <Image
-          className={classes.image}
-          src={coverUrl}
-          alt={alt}
-          onError={() => setShowDefaultCover(true)}
-          loading="lazy"
-          width={83}
-          height={120}
-        />
-      )}
-    </>
+export default function BookCoverImage({
+  coverUrl,
+  alt,
+  width = 83,
+  height = 120,
+}: BookCoverImageProps) {
+  const [isErrorImage, setIsErrorImage] = useState(false);
+  const showDefaultCover = isErrorImage || !coverUrl;
+  return showDefaultCover ? (
+    <BookDefaultCover />
+  ) : (
+    <Image
+      className={classes.image}
+      src={coverUrl}
+      alt={alt}
+      onError={() => setIsErrorImage(true)}
+      loading="lazy"
+      width={width}
+      height={height}
+    />
   );
 }
