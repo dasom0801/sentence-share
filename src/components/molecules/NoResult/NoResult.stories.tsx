b1d30277ad600cf42538a50d7357d96dd322e06f
Dasom <dev.dasom1012@gmail.com>
2025-07-06 14:30:19 +0900

    test: NoResult 컴포넌트 테스트 코드 추가

--- src/components/molecules/NoResult/NoResult.stories.tsx
+++ src/components/molecules/NoResult/NoResult.stories.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@mui/material';
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { expect } from 'storybook/test';
 import NoResult from '.';
 
 const meta = {
@@ -8,12 +10,53 @@ const meta = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-export const Default: Story = {
-  args: {},
+
+const args = {
+  title: '결과 없음',
+  description: '"검색어"에 대한 결과가 없습니다. 다시 검색해보세요.',
 };
 
-export const Description: Story = {
+export const TitleAndDescription: Story = {
+  args,
+  play: ({ canvas }) => {
+    expect(canvas.getByText(args.title)).toBeInTheDocument();
+    expect(canvas.getByText(args.description)).toBeInTheDocument();
+  },
+};
+
+export const Title: Story = {
   args: {
-    description: '"검색어"에 대한 결과가 없습니다. 다시 검색해보세요.',
+    title: args.title,
+  },
+  play: ({ canvas }) => {
+    expect(canvas.getByText(args.title)).toBeInTheDocument();
+    const description = canvas.queryByText(args.description);
+    expect(description).not.toBeInTheDocument();
+  },
+};
+
+export const DescriptionOnly: Story = {
+  args: {
+    description: args.description,
+  },
+  play: ({ canvas }) => {
+    expect(canvas.queryByText(args.title)).not.toBeInTheDocument();
+    expect(canvas.getByText(args.description)).toBeInTheDocument();
+  },
+};
+
+export const WithChildren: Story = {
+  args,
+  render: (args) => {
+    return (
+      <NoResult {...args}>
+        <Button>재시도</Button>
+      </NoResult>
+    );
+  },
+  play: ({ canvas }) => {
+    expect(canvas.getByText(args.title)).toBeInTheDocument();
+    expect(canvas.getByText(args.description)).toBeInTheDocument();
+    expect(canvas.getByRole('button', { name: '재시도' })).toBeInTheDocument();
   },
 };
