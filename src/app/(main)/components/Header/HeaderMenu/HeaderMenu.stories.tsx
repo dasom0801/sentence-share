6280fcfe385a4ddb2a057cf10541acad81f4fe94
Dasom <dev.dasom1012@gmail.com>
2025-07-07 21:49:31 +0900

    test: Header 컴포넌트 테스트 추가

--- src/app/(main)/components/Header/HeaderMenu/HeaderMenu.stories.tsx
+++ src/app/(main)/components/Header/HeaderMenu/HeaderMenu.stories.tsx
@@ -1,16 +1,28 @@
 import { MockUser } from '@/mocks/data';
+import { useUserStore } from '@/store/user';
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import HeaderMenu from '.';
 
 const meta = {
   title: 'layout/HeaderMenu',
   component: HeaderMenu,
+  args: {},
 } satisfies Meta<typeof HeaderMenu>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-export const Default: Story = {
-  args: {
-    user: MockUser,
+export const Login: Story = {
+  render: () => {
+    const state = useUserStore.getState();
+    useUserStore.setState({ ...state, user: MockUser });
+    return <HeaderMenu />;
+  },
+};
+
+export const Logout: Story = {
+  render: () => {
+    const state = useUserStore.getState();
+    useUserStore.setState({ ...state, user: null });
+    return <HeaderMenu />;
   },
 };
