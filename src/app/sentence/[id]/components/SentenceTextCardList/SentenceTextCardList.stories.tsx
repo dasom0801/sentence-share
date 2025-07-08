45e44ee2aa8715db83946b473f24e3ad487385e8
Dasom <dev.dasom1012@gmail.com>
2025-07-09 01:00:45 +0900

    test: SentenceTextCard 컴포넌트 테스트 추가

--- src/app/sentence/[id]/components/SentenceTextCardList/SentenceTextCardList.stories.tsx
+++ src/app/sentence/[id]/components/SentenceTextCardList/SentenceTextCardList.stories.tsx
@@ -1,5 +1,7 @@
 import { MockSentence } from '@/mocks/data';
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { getRouter } from '@storybook/nextjs-vite/navigation.mock';
+import { expect } from 'storybook/test';
 import SentenceTextCardList from '.';
 
 const meta = {
@@ -15,5 +17,27 @@ export const Default: Story = {
       ...MockSentence,
       _id: `${index}`,
     })),
+  },
+  play: async ({ canvas, step, userEvent }) => {
+    const listitems = canvas.getAllByRole('listitem');
+    await step('목록이 화면에 렌더링된다.', () => {
+      expect(listitems).toBeDefined();
+      expect(listitems.length).toBe(6);
+    });
+    await step(
+      '목록 아이템을 클릭하면 문장 상세 페이지로 이동한다.',
+      async () => {
+        const link = canvas
+          .getAllByRole('link')
+          .find((link) => link.getAttribute('href') === '/sentence/1');
+        expect(link).toBeDefined();
+        if (link) {
+          await userEvent.click(link);
+          expect(getRouter().push).toHaveBeenCalledWith(`/sentence/1`, {
+            scroll: true,
+          });
+        }
+      },
+    );
   },
 };
