81003d595027f41084bfa436903a8594bbdb3a47
Dasom <dev.dasom1012@gmail.com>
2025-08-12 17:15:59 +0900

    refactor: request cookie 사용 및 로그인 후 쿼리 파라미터 유지

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,26 @@
-import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
+
+const redirectToLogin = (request: NextRequest) => {
+  const loginUrl = new URL('/login', request.url);
+  const currentPath = request.nextUrl.pathname + request.nextUrl.search;
+  loginUrl.searchParams.set('afterLogin', currentPath);
+
+  return NextResponse.redirect(loginUrl);
+};
 
 export const config = {
   matcher: ['/my/:path*', '/edit/:path*'], // 로그인이 필요한 경로
 };
 
 export function middleware(request: NextRequest) {
-  const cookie = cookies();
-  const token = cookie.get('access_token');
+  // 정적 파일은 통과
+  if (request.nextUrl.pathname.includes('.')) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get('access_token');
   if (!token) {
-    return NextResponse.redirect(
-      new URL(`/login?afterLogin=${request.nextUrl.pathname}`, request.url),
-    );
+    return redirectToLogin(request);
   }
   return NextResponse.next();
 }
